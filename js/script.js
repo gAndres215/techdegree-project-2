5a88ba87f1d00f3df10125a278e2ad0299d01c56
Giovanni Hernandez <ghernandez9319@gmail.com>
2020-03-07 09:26:20 -0700

    Meets expectations mark

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ const itemsPerPage = 10;
 
 
 
+
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
@@ -93,6 +94,60 @@ appendPageLinks(listItems);
 // Links are being displayed. Next add a event listener to the links to display the new page with the link number and the 
 // being passed as parameters
 
+function createSearchBar (){
+   let headerDiv = document.querySelector('.page-header');
+   let searchDiv = document.createElement('div');
+   let searchBar = document.createElement('input');
+   let searchButton = document.createElement('button');
+
+   searchDiv.className = 'student-search';
+   searchBar.placeholder = 'Search for Students';
+   searchButton.textContent = 'Search';
+
+   headerDiv.appendChild(searchDiv);
+   searchDiv.appendChild(searchBar);
+   searchDiv.appendChild(searchButton);
+
+   searchBar.addEventListener('keyup', () =>{
+      performSearch(event.target.value);
+     // console.log(event.target.value);
+
+      //1. add functionality to the search bar
+      //2. how to dynamically display?
+      // create a new array with found items and pass to showpage()?
+   })
+
+   searchButton.addEventListener('click', () =>{
+      performSearch(searchBar.value);
+      // console.log(searchBar.value);
+   })
+
+   
+}
+function performSearch (searchInput) {
+   let liArray = [];
+   let names = document.querySelectorAll('h3');
+   for(let i = 0; i < names.length; i++){
+     
+      // let name = names[i].textContent;
+      //console.log(name);
+      if(searchInput.length !== 0 && names[i].textContent.toLowerCase().includes(searchInput.toLowerCase())){
+        //liArray.push(names[i].parentElement.parentElement);
+        
+   
+   }
+   
+   showPage(liArray, 1);
+   // for(let x = 0; x < name.length; x++){
+   // name[x].display = 'none';
+     
+   //   if(input.textContent.length !== 0 && name[x].textContent.toLowerCase().includes(input.textContent.toLowerCase())){
+   //     name[x].display = '';
+   //      }
+   
+   // }
+ }
 
 
+createSearchBar();
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
